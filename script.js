800284b9b08214664ca6a50dcacff9a350a2aca6
matterladd <matterlad@protonmail.com>
2024-11-13 11:35:57 -0500

    This works in getting an access token, same as API key?

--- script.js
+++ script.js
@@ -1,28 +1,43 @@
 /**
- * Using Authorization code flow, attempt at PKCE (not used) found in authPKCE.js
- * https://developer.spotify.com/documentation/web-api/tutorials/code-flow
-*/ 
-require('dotenv').config();
-const express = require('express');
+ * Using Authorization code flow instead of PKCE, attempt (unused) can be found in authPKCE.js
+ */
 
-var client_id = process.env.client_id;
-// var client_secret = process.env.client_secret;
-var redirect_uri = 'http://localhost:8888/callback';
+require('dotenv').config()
+const axios = require('axios');
+const qs = require('querystring');
 
-var app = express();
+// Your Client ID and Client Secret from the Spotify Developer Dashboard
+const CLIENT_ID = process.env.CLIENT_ID; 
+const CLIENT_SECRET = process.env.CLIENT_SECRET;
 
-app.get('/login', function(req, res) {
+// The URL for Spotify's authorization API
+const TOKEN_URL = 'https://accounts.spotify.com/api/token';
 
-  var state = generateRandomString(16);
-  console.log(state);
-  var scope = 'user-read-private user-read-email';
+// Encode the Client ID and Client Secret in base64 for basic auth
+const auth = Buffer.from(`${CLIENT_ID}:${CLIENT_SECRET}`).toString('base64');
 
-  res.redirect('https://accounts.spotify.com/authorize?' +
-    querystring.stringify({
-      response_type: 'code',
-      client_id: client_id,
-      scope: scope,
-      redirect_uri: redirect_uri,
-      state: state
-    }));
+// Request the token from Spotify
+async function getAccessToken() {
+    try {
+        // Send a POST request to get the access token
+        const response = await axios.post(TOKEN_URL, qs.stringify({ grant_type: 'client_credentials' }), {
+            headers: {
+                'Authorization': `Basic ${auth}`,
+                'Content-Type': 'application/x-www-form-urlencoded'
+            }
+        });
+
+        // Extract the access token from the response
+        const accessToken = response.data.access_token;
+        console.log('Access Token:', accessToken);
+
+        return accessToken;
+    } catch (error) {
+        console.error('Error getting access token:', error);
+    }
+}
+
+// Token usage
+getAccessToken().then((token) => {
+    // Now you can use the token to make requests to the Spotify API
 });
